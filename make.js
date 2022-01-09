fad08187dbed6320391b5f3fe263536aa896510d
Mike Perry <mike@utahcounty.gov>
2022-01-09 14:04:22 -0700

    beginning 'make' script

---
+++ make.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let rl = require("readline-sync");
+
+(async () => {
+  console.log("Don't use backspace");
+  let date = rl.question("\nWhen was this class? (e.g. 2021-12-27) ");
+  let country = rl.question("\n? ");
+  console.log(`${name}, is a citizen of ${country}`);
+
+  console.log("\nScript processed successfully");
+  process.exit(0);
+})();
+
+// ---
+// title: Study Recap (1 Nephi 4-5)
+// date: 2021-12-20
+// slug: /1-nephi-4-5
+// ---
+
+// We covered 1 Nephi 4-5.
+
+// [📹 Video](https://www.youtube.com/watch?v=3v1MUltwSFM)
+
